157c877df0d8565f5cab27539b3ad7afd9fcf5aa
Jeremy Busk <jeremybusk@gmail.com>
2018-03-20 16:55:33 -0600

    modified

--- ci/build-subprojects.sh
+++ ci/build-subprojects.sh
@@ -1,11 +1,11 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
 if [ -d "${SUBPROJECT}" -a -f "${SUBPROJECT}/build.sh" ]; then
-    echo "${SUBPROJECT}/build.sh"
-    #${SUBPROJECT}/build.sh
-    (cd "${SUBPROJECT}"; bash ./build.sh)
+    #echo "${SUBPROJECT}/build.sh"
+    ${SUBPROJECT}/build.sh
+    # (cd "${SUBPROJECT}"; bash ./build.sh)
 elif [ -f "build.sbt" ]; then
     echo "build.sbt"
 else
