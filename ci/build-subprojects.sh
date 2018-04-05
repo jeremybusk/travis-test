bc39956ba29c8c177631e2516c9bfb8c98e4e229
jeremybusk <jeremy@pyrofex.net>
2018-04-04 22:10:08 -0600

    Update build-subprojects.sh

--- ci/build-subprojects.sh
+++ ci/build-subprojects.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 
-#set -euo pipefail
-#if [[ $TRAVIS = "true" ]]; then
-#    echo "yes running on travis"
-#else
-#    echo "no travis"
-#fi
 
-#sleep 1000
-docker run -dit -v /var/run/docker.sock:/var/run/docker.sock --name pusher ubuntu:16.04
-docker cp test-travis.sh pusher:/
-docker exec -it pusher /bin/bash -c "./test-travis.sh"
-exit 1
+
 
 if [ -d "${SUBPROJECT}" -a -f "${SUBPROJECT}/build.sh" ]; then
-    ${SUBPROJECT}/build.sh
+    # ${SUBPROJECT}/build.sh
+    echo "start"
+    docker run -dit -v /var/run/docker.sock:/var/run/docker.sock --name pusher ubuntu:16.04
+    docker cp test-travis.sh pusher:/
+    docker exec -it pusher /bin/bash -c "./test-travis.sh"
+# exit 1
 elif [ -f "build.sbt" ]; then
     echo "build.sbt"
 else
