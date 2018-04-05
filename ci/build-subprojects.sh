9102e7b7c5b2f2aa43576add885bb625dc3d56a4
Jeremy Busk <jeremybusk@gmail.com>
2018-04-05 07:05:54 -0600

    test away

--- ci/build-subprojects.sh
+++ ci/build-subprojects.sh
@@ -12,7 +12,7 @@ if [ -d "${SUBPROJECT}" -a -f "${SUBPROJECT}/build.sh" ]; then
     echo ${CI}
     echo ${TRAVIS}
     echo "=========="
-docker run -dit -v /var/run/docker.sock:/var/run/docker.sock -e DOCKER_USERNAME="${DOCKER_USERNAME}" -e DOCKER_PASSWORD="${DOCKER_PASSWORD}" -e TRAVIS="${TRAVIS}" --name ${pusher_docker_name} ubuntu:16.04
+docker run -dit -v /var/run/docker.sock:/var/run/docker.sock -e DOCKER_USERNAME="${DOCKER_USERNAME}" -e DOCKER_PASSWORD="${DOCKER_PASSWORD}" -e TRAVIS="${TRAVIS}" --name pusher ubuntu:16.04
     docker run -dit --name pusher ubuntu:16.04
     docker cp test-travis.sh pusher:/
     docker exec -it pusher /bin/bash -c "./test-travis.sh"
